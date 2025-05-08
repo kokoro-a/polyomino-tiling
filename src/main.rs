9b660aaf1bca2d23ab712155e3912eb6fa07a3e6
kokokokoko <k.arai.h963@gmail.com>
2025-05-08 13:43:33 +0000

    F

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,58 @@ struct DancingLinks {
 
 impl DancingLinks {
     fn new() -> Self {
-        let mut root = ColumnNode {
+        let mut root = Box::new(ColumnNode {
             name: "root".to_string(),
             size: 0,
             left: std::ptr::null_mut(),
             right: std::ptr::null_mut(),
             head: std::ptr::null_mut(),
-        };
-        root.left = &root as *const _ as *mut ColumnNode;
-        root.right = &root as *const _ as *mut ColumnNode;
-        DancingLinks {
-            root: &root as *const _ as *mut ColumnNode,
+        });
+        root.left = &mut *root;
+        root.right = &mut *root;
+
+        Self {
+            root: Box::into_raw(root),
+        }
+    }
+
+    fn add_column(&mut self, name: String) {
+        /*
+        Before add_column:
+            root -- column1 -- ... -- columnN -- root
+
+        After add_column:
+            root -- column1 -- ... -- columnN -- **new_column** --root
+        */
+        unsafe {
+            let last_column_before_add = (*self.root).left;
+            let new_column = Box::new(ColumnNode {
+                name,
+                size: 0,
+                left: last_column_before_add,
+                right: self.root,
+                head: std::ptr::null_mut(),
+            });
+
+            let new_column_ptr = Box::into_raw(new_column);
+            (*last_column_before_add).right = new_column_ptr;
+            (*self.root).left = new_column_ptr;
+        }
+    }
+}
+
+impl Drop for DancingLinks {
+    fn drop(&mut self) {
+        unsafe {
+            let mut current = (*self.root).right;
+            while current != self.root {
+                let next = (*current).right;
+                println!("Dropping column: {}", (*current).name);
+                drop(Box::from_raw(current));
+                current = next;
+            }
+            println!("Dropping root");
+            drop(Box::from_raw(self.root));
         }
     }
 }
@@ -37,7 +78,9 @@ struct Node {
 }
 
 fn main() {
-    let dlx = DancingLinks::new();
+    let mut dlx = DancingLinks::new();
+    dlx.add_column("Column 1".to_string());
+    dlx.add_column("Column 2".to_string());
     // Example usage of the Dancing Links algorithm
     // Add columns and rows to the dlx structure
     // Perform search and cover/uncover operations
