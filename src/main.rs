f522b8af44249b06ceac2516923f9baba8184b5e
kokokokoko <k.arai.h963@gmail.com>
2025-05-08 14:33:24 +0000

    F

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,80 @@ impl DancingLinks {
             (*self.root).left = new_column_ptr;
         }
     }
+
+    fn add_row(&mut self, row: Vec<isize>) {
+        let mut current_column = unsafe { (*self.root).right };
+        let mut previous_node: *mut Node = std::ptr::null_mut();
+        for &x in row.iter() {
+            if x == 1 {
+                let new_node: Box<Node> = Box::new(Node {
+                    row: 0, // Placeholder for row number
+                    column: current_column,
+                    up: std::ptr::null_mut(),
+                    down: std::ptr::null_mut(),
+                    left: std::ptr::null_mut(),
+                    right: std::ptr::null_mut(),
+                });
+                let new_node_ptr = Box::into_raw(new_node);
+                unsafe {
+                    (*current_column).size += 1;
+
+                    /* link vertical */
+                    let head_node = (*current_column).head;
+                    if head_node.is_null() {
+                        (*current_column).head = new_node_ptr;
+                        (*new_node_ptr).up = new_node_ptr;
+                        (*new_node_ptr).down = new_node_ptr;
+                    } else {
+                        let last_node_before_add = (*head_node).up;
+                        (*last_node_before_add).down = new_node_ptr;
+                        (*new_node_ptr).up = last_node_before_add;
+                        (*new_node_ptr).down = head_node;
+                        (*head_node).up = new_node_ptr;
+                    }
+
+                    /* link horizontal */
+                    (*new_node_ptr).left = previous_node;
+                    if !previous_node.is_null() {
+                        (*new_node_ptr).left = previous_node;
+                        (*previous_node).right = new_node_ptr;
+                    }
+                }
+                previous_node = new_node_ptr;
+            }
+            current_column = unsafe { (*current_column).right };
+        }
+        let rightmost_node = previous_node;
+        let leftmost_node = {
+            let mut current_node = rightmost_node;
+            unsafe {
+                while !(*current_node).left.is_null() {
+                    current_node = (*current_node).left;
+                }
+            }
+            current_node
+        };
+        unsafe {
+            (*leftmost_node).left = rightmost_node;
+            (*rightmost_node).right = leftmost_node;
+        }
+    }
+
+    fn drop_column(&mut self, column: *mut ColumnNode) {
+        unsafe {
+            if !(*column).head.is_null() {
+                let head = (*column).head;
+                let mut current = (*head).down;
+                while current != head {
+                    let next = (*current).down;
+                    drop(Box::from_raw(current));
+                    current = next;
+                }
+                drop(Box::from_raw(head));
+            }
+            drop(Box::from_raw(column));
+        }
+    }
 }
 
 impl Drop for DancingLinks {
@@ -51,7 +125,7 @@ impl Drop for DancingLinks {
             while current != self.root {
                 let next = (*current).right;
                 println!("Dropping column: {}", (*current).name);
-                drop(Box::from_raw(current));
+                self.drop_column(current);
                 current = next;
             }
             println!("Dropping root");
@@ -81,6 +155,8 @@ fn main() {
     let mut dlx = DancingLinks::new();
     dlx.add_column("Column 1".to_string());
     dlx.add_column("Column 2".to_string());
+    dlx.add_row(vec![1, 0]);
+    dlx.add_row(vec![0, 1]);
     // Example usage of the Dancing Links algorithm
     // Add columns and rows to the dlx structure
     // Perform search and cover/uncover operations
