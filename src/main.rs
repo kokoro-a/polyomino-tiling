6c0a1c2fd2d78a64fb2cfab109fdb4678d13e7c2
kokokokoko <k.arai.h963@gmail.com>
2025-05-16 13:58:19 +0000

    F

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,14 @@ use std::iter;
 
 struct DancingLinks {
     root: *mut ColumnNode,
-    n_rows: isize,
-    n_cols: isize,
+    n_rows: usize,
+    n_cols: usize,
     columns: Vec<*mut ColumnNode>,
 }
 
 impl DancingLinks {
     fn new() -> Self {
-        let mut root: Box<ColumnNode> = Box::new(ColumnNode {
-            index: 0,
-            size: 0,
-            left: std::ptr::null_mut(),
-            right: std::ptr::null_mut(),
-            head: std::ptr::null_mut(),
-        });
+        let mut root: Box<ColumnNode> = Box::new(ColumnNode::new(0));
         root.left = &mut *root;
         root.right = &mut *root;
 
@@ -28,52 +22,35 @@ impl DancingLinks {
         }
     }
 
-    fn solve(&mut self, solution: Vec<usize>) -> Result<Vec<usize>, String> {
-        let column_minimum = self
-            .columns
-            .iter()
-            .copied()
-            .min_by_key(|&col| unsafe { (*col).size });
-
-        match column_minimum {
-            Some(column) => unsafe {
-                (*column).cover();
-                let mut current = (*column).head;
-                while current != column {
-                    let row_index = (*current).row_index;
-                    let mut node = current;
-                    while node != column {
-                        let col = (*node).column;
-                        (*col).cover();
-                        node = (*node).right;
+    fn solve(&mut self, solution: Vec<usize>) -> Option<Vec<usize>> {
+        let columns_sorted = {
+            let cs = self.columns.clone();
+            cs.sort_by_key(|&col| unsafe { (*col).size });
+            cs
+        };
+        columns_sorted.iter().for_each(|&col| unsafe {
+            // Cover the column
+            (*col).cover();
+            (*col).iterate_column().for_each(|node| {
+                let solution_candidate = (*node).row_index;
+                let mut current = (*node).right;
+                iter::from_fn(move || {
+                    if current == node {
+                        None
+                    } else {
+                        let next = (*current).right;
+                        Some(current)
                     }
-                    self.solve();
-                    // Uncover the columns and rows
-                    node = current;
-                    while node != column {
-                        let col = (*node).column;
-                        (*col).uncover();
-                        node = (*node).right;
-                    }
-                }
-                (*column).uncover();
-            },
-            None => {
-                return Ok(solution);
-            }
-        }
+                })
+                .for_each(|node| unsafe { (*node).unlink_vertically() });
+            })
+        });
     }
 
     fn append_column(&mut self) {
         unsafe {
             let old_rightmost = (*self.root).left;
-            let new_column: Box<ColumnNode> = Box::new(ColumnNode {
-                index: self.n_cols,
-                size: 0,
-                left: std::ptr::null_mut(),
-                right: std::ptr::null_mut(),
-                head: std::ptr::null_mut(),
-            });
+            let new_column: Box<ColumnNode> = Box::new(ColumnNode::new(self.n_cols));
             let new_column_ptr: *mut ColumnNode = Box::into_raw(new_column);
             (*old_rightmost).insert_right(new_column_ptr);
             self.n_cols += 1;
@@ -92,14 +69,7 @@ impl DancingLinks {
             .filter(|&(_, &val)| val != 0)
             .map(|(i, _)| {
                 let column = self.columns[i];
-                let new_node: Box<Node> = Box::new(Node {
-                    row_index: self.n_rows,
-                    column: column,
-                    up: std::ptr::null_mut(),
-                    down: std::ptr::null_mut(),
-                    left: std::ptr::null_mut(),
-                    right: std::ptr::null_mut(),
-                });
+                let new_node: Box<Node> = Box::new(Node::new(self.n_rows, column));
                 let new_node_ptr = Box::into_raw(new_node);
                 unsafe { (*column).append_node(new_node_ptr) };
                 new_node_ptr
@@ -153,7 +123,7 @@ impl Drop for DancingLinks {
 }
 
 struct ColumnNode {
-    index: isize,
+    index: usize,
     size: usize,
     left: *mut ColumnNode,
     right: *mut ColumnNode,
@@ -161,6 +131,16 @@ struct ColumnNode {
 }
 
 impl ColumnNode {
+    fn new(index: usize) -> Self {
+        Self {
+            index,
+            size: 0,
+            left: std::ptr::null_mut(),
+            right: std::ptr::null_mut(),
+            head: std::ptr::null_mut(),
+        }
+    }
+
     fn insert_right(&mut self, new_column: *mut ColumnNode) {
         unsafe {
             if new_column.is_null() {
@@ -210,23 +190,43 @@ impl ColumnNode {
     }
 
     fn cover(&mut self) {
-        let left = (*self).left;
-        let right = (*self).right;
+        self.unlink();
+        self.iterate_column().for_each(|node| unsafe {
+            (*node).unlink_horizontally();
+
+            let mut current = node;
+            iter::from_fn(move || {
+                current = (*current).left;
+                if current == node { None } else { Some(current) }
+            })
+            .for_each(|node| {
+                (*node).unlink_vertically();
+                (*(*node).column).size -= 1;
+            });
+        });
+    }
+
+    fn unlink(&mut self) {
         unsafe {
+            let left = (*self).left;
+            let right = (*self).right;
             (*left).right = right;
             (*right).left = left;
         }
-        self.iterate_column().for_each(|node| unsafe {
-            let left = (*node).left;
-            let right = (*node).right;
-            (*left).right = right;
-            (*right).left = left;
-        });
+    }
+
+    fn relink(&mut self) {
+        unsafe {
+            let left = (*self).left;
+            let right = (*self).right;
+            (*right).left = self;
+            (*left).right = self;
+        }
     }
 }
 
 struct Node {
-    row_index: isize,
+    row_index: usize,
     column: *mut ColumnNode,
     up: *mut Node,
     down: *mut Node,
@@ -235,6 +235,16 @@ struct Node {
 }
 
 impl Node {
+    fn new(row_index: usize, column: *mut ColumnNode) -> Self {
+        Self {
+            row_index,
+            column,
+            up: std::ptr::null_mut(),
+            down: std::ptr::null_mut(),
+            left: std::ptr::null_mut(),
+            right: std::ptr::null_mut(),
+        }
+    }
     fn insert_down(&mut self, new_node: *mut Node) {
         unsafe {
             if new_node.is_null() {
@@ -276,6 +286,44 @@ impl Node {
     fn insert_left(&mut self, new_node: *mut Node) {
         unsafe {
             (*self.left).insert_right(new_node);
+        }
+    }
+
+    fn unlink_vertically(&mut self) {
+        unsafe {
+            let up = (*self).up;
+            let down = (*self).down;
+            (*up).down = down;
+            (*down).up = up;
+            (*(*self).column).size -= 1;
+        }
+    }
+
+    fn unlink_horizontally(&mut self) {
+        unsafe {
+            let left = (*self).left;
+            let right = (*self).right;
+            (*left).right = right;
+            (*right).left = left;
+        }
+    }
+
+    fn relink_vertically(&mut self) {
+        unsafe {
+            let up = (*self).up;
+            let down = (*self).down;
+            (*up).down = self;
+            (*down).up = self;
+            (*(*self).column).size += 1;
+        }
+    }
+
+    fn relink_horizontally(&mut self) {
+        unsafe {
+            let left = (*self).left;
+            let right = (*self).right;
+            (*left).right = self;
+            (*right).left = self;
         }
     }
 }
